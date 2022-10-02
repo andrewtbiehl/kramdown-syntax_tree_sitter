66910f24838be4e7f6508e7451309eba09df2c2b
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-10-02 15:21:20 -0500

    Add Kramdown as a runtime dependency
    Also adds a test that verifies Kramdown can render Markdown to HTML
    without highlighting code.

--- kramdown-syntax_tree_sitter.gemspec
+++ kramdown-syntax_tree_sitter.gemspec
@@ -11,6 +11,8 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = '>= 2.7'
   spec.homepage = 'https://github.com/andrewtbiehl/kramdown-syntax_tree_sitter'
 
+  spec.add_runtime_dependency 'kramdown', '~> 2.0'
+
   spec.add_development_dependency 'minitest', '~> 5.0'
   spec.add_development_dependency 'rake', '~> 13.0'
   spec.add_development_dependency 'rubocop', '~> 1.36'
--- test/kramdown/test_syntax_tree_sitter.rb
+++ test/kramdown/test_syntax_tree_sitter.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
+require 'kramdown'
 require 'test_helper'
+
+PYTHON_MARKDOWN = <<~MARKDOWN
+  ~~~python
+  print('Hello, World!')
+  ~~~
+MARKDOWN
+
+PYTHON_NO_HIGHLIGHT_HTML = <<~HTML
+  <pre><code class="language-python">print('Hello, World!')
+  </code></pre>
+HTML
 
 module Kramdown
   class TestSyntaxHighlighting < Minitest::Test
     def test_that_tree_sitter_has_a_version_number
       refute_nil Converter::SyntaxHighlighter::TreeSitter::VERSION
+    end
+
+    def test_that_it_can_use_no_highlighting
+      actual = Document.new(PYTHON_MARKDOWN, syntax_highlighter: nil).to_html
+      assert_equal actual, PYTHON_NO_HIGHLIGHT_HTML
     end
   end
 end
