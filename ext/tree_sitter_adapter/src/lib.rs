3aa92c6585bc40537876f1ec7aa78f7314201c58
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-10-15 15:00:22 -0500

    Remove unneeded whitespace

--- ext/tree_sitter_adapter/src/lib.rs
+++ ext/tree_sitter_adapter/src/lib.rs
@@ -10,7 +10,6 @@ methods!(
     _rtself,
     fn pub_reverse(input: RString) -> RString {
         let ruby_string = input.map_err(VM::raise_ex).unwrap();
-
         RString::new_utf8(&ruby_string.to_string().chars().rev().collect::<String>())
     }
 );
