f8b34c0d4b70745c50b97f3533e79b22bc3a9dc3
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-10-15 14:59:55 -0500

    Remove redundant closure

--- ext/tree_sitter_adapter/src/lib.rs
+++ ext/tree_sitter_adapter/src/lib.rs
@@ -9,7 +9,7 @@ methods!(
     RutieExample,
     _rtself,
     fn pub_reverse(input: RString) -> RString {
-        let ruby_string = input.map_err(|e| VM::raise_ex(e)).unwrap();
+        let ruby_string = input.map_err(VM::raise_ex).unwrap();
 
         RString::new_utf8(&ruby_string.to_string().chars().rev().collect::<String>())
     }
