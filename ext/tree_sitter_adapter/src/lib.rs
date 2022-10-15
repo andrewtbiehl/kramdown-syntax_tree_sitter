e45cabe21aa44b7986a7998fb3718811c0e666b4
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-10-15 18:28:39 -0500

    Use tree sitter_adapter package to highlight code
    Adds a function that allows the tree_sitter_adapter library to escape
    text, updates the main library method, updates the gem to invoke the
    library in order to highlight code, and removes a test rendered obsolete
    by these changes.
    
    With this change, the Ruby and Rust components of the gem are fully
    integrated. With this change, essentially the only MVP requirement
    remaining  is building out the Tree-sitter adapter library completely.

--- ext/tree_sitter_adapter/src/lib.rs
+++ ext/tree_sitter_adapter/src/lib.rs
@@ -3,8 +3,24 @@ extern crate rutie;
 
 use rutie::{Class, Object, RString};
 
-fn reverse(s: &str) -> String {
-    s.chars().rev().collect()
+// Escapes HTML text content.
+//
+// Not intended for use on other HTML content, such as attribute content.
+fn escape_text_html(text: &str) -> String {
+    let mut escaped_text = String::new();
+    for c in text.chars() {
+        match c {
+            '&' => escaped_text.push_str("&amp;"),
+            '<' => escaped_text.push_str("&lt;"),
+            '>' => escaped_text.push_str("&gt;"),
+            _ => escaped_text.push(c),
+        }
+    }
+    escaped_text
+}
+
+fn highlight(code: &str) -> String {
+    escape_text_html(code)
 }
 
 class!(TreeSitterAdapter);
@@ -12,14 +28,14 @@ class!(TreeSitterAdapter);
 methods!(
     TreeSitterAdapter,
     _rtself,
-    fn pub_reverse(raw_text: RString) -> RString {
-        RString::new_utf8(&reverse(&raw_text.unwrap().to_string()))
+    fn pub_highlight(raw_code: RString) -> RString {
+        RString::new_utf8(&highlight(&raw_code.unwrap().to_string()))
     }
 );
 
 #[no_mangle]
 pub extern "C" fn Init_tree_sitter_adapter() {
     Class::new("TreeSitterAdapter", None).define(|class_| {
-        class_.def_self("reverse", pub_reverse);
+        class_.def_self("highlight", pub_highlight);
     });
 }
--- lib/kramdown/converter/syntax_highlighter/tree_sitter.rb
+++ lib/kramdown/converter/syntax_highlighter/tree_sitter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'kramdown'
+require 'tree_sitter_adapter'
 
 module Kramdown
   module Converter # rubocop:disable Style/Documentation
@@ -10,8 +11,8 @@ module Kramdown
       # Currently it merely escapes the code so that it can be safely inserted into HTML
       # text.
       module TreeSitter
-        def self.call(converter, raw_text, _, type, _)
-          rendered_text = converter.escape_html raw_text
+        def self.call(_, raw_text, _, type, _)
+          rendered_text = TreeSitterAdapter.highlight raw_text
           # Code blocks are additionally wrapped in HTML code tags
           type == :block ? "<pre><code>#{rendered_text}</code></pre>" : rendered_text
         end
