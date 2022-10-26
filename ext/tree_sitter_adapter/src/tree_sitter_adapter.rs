0150838f863914c39e20fa43307131caf8044dca
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-10-26 07:21:35 -0700

    Extract HTML text character escaping into function

--- ext/tree_sitter_adapter/src/tree_sitter_adapter.rs
+++ ext/tree_sitter_adapter/src/tree_sitter_adapter.rs
@@ -1,15 +1,20 @@
+// Escape a single character for HTML text.
+//
+// Not intended for use on other HTML content, such as attribute content.
+fn html_text_escape(c: char) -> String {
+    match c {
+        '&' => "&amp;".to_string(),
+        '<' => "&lt;".to_string(),
+        '>' => "&gt;".to_string(),
+        _ => c.to_string(),
+    }
+}
+
 // Escapes HTML text content.
 //
 // Not intended for use on other HTML content, such as attribute content.
 fn escape_text_html(text: &str) -> String {
-    text.chars()
-        .map(|c| match c {
-            '&' => "&amp;".to_string(),
-            '<' => "&lt;".to_string(),
-            '>' => "&gt;".to_string(),
-            _ => c.to_string(),
-        })
-        .collect()
+    text.chars().map(html_text_escape).collect()
 }
 
 pub fn highlight(code: &str) -> String {
