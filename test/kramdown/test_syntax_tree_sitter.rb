0a4a5d12287f26c38f69b0490b5c5f68f6cc2e55
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2023-02-01 21:05:40 -0600

    Refactor test suite via storing test data in map
    Also updates an existing note in the (unreleased portion of the)
    changelog to describe both this and another recent test suite refactor.

--- test/kramdown/test_syntax_tree_sitter.rb
+++ test/kramdown/test_syntax_tree_sitter.rb
@@ -12,48 +12,31 @@ def read_test_file(filename)
   File.read full_path
 end
 
-PYTHON_STANDARD_MARKDOWN = read_test_file 'python_standard.md'
-
-PYTHON_TREE_SITTER_MARKDOWN = read_test_file 'python_tree_sitter.md'
-
-PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN = \
-  read_test_file 'python_tree_sitter_escape_character.md'
-
-HTML_TREE_SITTER_MARKDOWN = read_test_file 'html_tree_sitter.md'
-
-PYTHON_TREE_SITTER_INLINE_MARKDOWN = read_test_file 'python_tree_sitter_inline.md'
-
-BAD_SYNTAX_PYTHON_TREE_SITTER_MARKDOWN = \
-  read_test_file 'bad_syntax_python_tree_sitter.md'
-
-NO_LANGUAGE_TREE_SITTER_MARKDOWN = read_test_file 'no_language_tree_sitter.md'
-
-NO_LANGUAGE_TREE_SITTER_INLINE_MARKDOWN = \
-  read_test_file 'no_language_tree_sitter_inline.md'
-
-PYTHON_NO_HIGHLIGHT_HTML = read_test_file 'python_no_highlight.html'
-
-PYTHON_ROUGE_HTML = read_test_file 'python_rouge.html'
-
-PYTHON_TREE_SITTER_HTML = read_test_file 'python_tree_sitter.html'
-
-PYTHON_TREE_SITTER_INLINE_CSS_HTML = read_test_file 'python_tree_sitter_inline_css.html'
-
-PYTHON_TREE_SITTER_CSS_CLASS_HTML = read_test_file 'python_tree_sitter_css_class.html'
-
-PYTHON_TREE_SITTER_INLINE_HTML = read_test_file 'python_tree_sitter_inline.html'
-
-HTML_TREE_SITTER_HTML = read_test_file 'html_tree_sitter.html'
-
-BAD_SYNTAX_PYTHON_TREE_SITTER_HTML = read_test_file 'bad_syntax_python_tree_sitter.html'
-
-NO_LANGUAGE_TREE_SITTER_HTML = read_test_file 'no_language_tree_sitter.html'
-
-NO_LANGUAGE_TREE_SITTER_INLINE_HTML = \
-  read_test_file 'no_language_tree_sitter_inline.html'
-
-PYTHON_TREE_SITTER_ROUGE_IDENTIFIER_HTML = \
-  read_test_file 'python_tree_sitter_rouge_identifier.html'
+TEST_DATA =
+  {
+    PYTHON_STANDARD_MARKDOWN: 'python_standard.md',
+    PYTHON_TREE_SITTER_MARKDOWN: 'python_tree_sitter.md',
+    PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN:
+      'python_tree_sitter_escape_character.md',
+    HTML_TREE_SITTER_MARKDOWN: 'html_tree_sitter.md',
+    PYTHON_TREE_SITTER_INLINE_MARKDOWN: 'python_tree_sitter_inline.md',
+    BAD_SYNTAX_PYTHON_TREE_SITTER_MARKDOWN: 'bad_syntax_python_tree_sitter.md',
+    NO_LANGUAGE_TREE_SITTER_MARKDOWN: 'no_language_tree_sitter.md',
+    NO_LANGUAGE_TREE_SITTER_INLINE_MARKDOWN: 'no_language_tree_sitter_inline.md',
+    PYTHON_NO_HIGHLIGHT_HTML: 'python_no_highlight.html',
+    PYTHON_ROUGE_HTML: 'python_rouge.html',
+    PYTHON_TREE_SITTER_HTML: 'python_tree_sitter.html',
+    PYTHON_TREE_SITTER_INLINE_CSS_HTML: 'python_tree_sitter_inline_css.html',
+    PYTHON_TREE_SITTER_CSS_CLASS_HTML: 'python_tree_sitter_css_class.html',
+    PYTHON_TREE_SITTER_INLINE_HTML: 'python_tree_sitter_inline.html',
+    HTML_TREE_SITTER_HTML: 'html_tree_sitter.html',
+    BAD_SYNTAX_PYTHON_TREE_SITTER_HTML: 'bad_syntax_python_tree_sitter.html',
+    NO_LANGUAGE_TREE_SITTER_HTML: 'no_language_tree_sitter.html',
+    NO_LANGUAGE_TREE_SITTER_INLINE_HTML: 'no_language_tree_sitter_inline.html',
+    PYTHON_TREE_SITTER_ROUGE_IDENTIFIER_HTML: 'python_tree_sitter_rouge_identifier.html'
+  }
+  .transform_values(&method(:read_test_file))
+  .freeze
 
 PYTHON_MISSING_LANGUAGE_PARSER_MSG = 'Error retrieving language configuration for ' \
                                      "scope 'source.python': Language not found"
@@ -79,101 +62,101 @@ module Kramdown
     end
 
     def test_that_it_can_use_no_highlighting
-      actual = convert_to_html PYTHON_STANDARD_MARKDOWN, nil
+      actual = convert_to_html TEST_DATA.fetch(:PYTHON_STANDARD_MARKDOWN), nil
 
-      assert_equal PYTHON_NO_HIGHLIGHT_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_NO_HIGHLIGHT_HTML), actual
     end
 
     def test_that_it_can_use_rouge_highlighting
-      actual = convert_to_html PYTHON_STANDARD_MARKDOWN, :rouge
+      actual = convert_to_html TEST_DATA.fetch(:PYTHON_STANDARD_MARKDOWN), :rouge
 
-      assert_equal PYTHON_ROUGE_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_ROUGE_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_highlighting
       actual = convert_to_html(
-        PYTHON_TREE_SITTER_MARKDOWN,
+        TEST_DATA.fetch(:PYTHON_TREE_SITTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal PYTHON_TREE_SITTER_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_TREE_SITTER_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_inline_css_highlighting
       actual = convert_to_html(
-        PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN,
+        TEST_DATA.fetch(:PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH, css_classes: false }
       )
 
-      assert_equal PYTHON_TREE_SITTER_INLINE_CSS_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_TREE_SITTER_INLINE_CSS_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_css_class_highlighting
       actual = convert_to_html(
-        PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN,
+        TEST_DATA.fetch(:PYTHON_TREE_SITTER_ESCAPE_CHARACTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH, css_classes: true }
       )
 
-      assert_equal PYTHON_TREE_SITTER_CSS_CLASS_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_TREE_SITTER_CSS_CLASS_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_inline_highlighting
       actual = convert_to_html(
-        PYTHON_TREE_SITTER_INLINE_MARKDOWN,
+        TEST_DATA.fetch(:PYTHON_TREE_SITTER_INLINE_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal PYTHON_TREE_SITTER_INLINE_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_TREE_SITTER_INLINE_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_html_escaped_highlighting
       actual = convert_to_html(
-        HTML_TREE_SITTER_MARKDOWN,
+        TEST_DATA.fetch(:HTML_TREE_SITTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal HTML_TREE_SITTER_HTML, actual
+      assert_equal TEST_DATA.fetch(:HTML_TREE_SITTER_HTML), actual
     end
 
     def test_that_it_can_use_tree_sitter_highlighting_on_bad_syntax
       actual = convert_to_html(
-        BAD_SYNTAX_PYTHON_TREE_SITTER_MARKDOWN,
+        TEST_DATA.fetch(:BAD_SYNTAX_PYTHON_TREE_SITTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal BAD_SYNTAX_PYTHON_TREE_SITTER_HTML, actual
+      assert_equal TEST_DATA.fetch(:BAD_SYNTAX_PYTHON_TREE_SITTER_HTML), actual
     end
 
     def test_that_it_can_use_no_highlighting_with_tree_sitter_enabled
       actual = convert_to_html(
-        NO_LANGUAGE_TREE_SITTER_MARKDOWN,
+        TEST_DATA.fetch(:NO_LANGUAGE_TREE_SITTER_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal NO_LANGUAGE_TREE_SITTER_HTML, actual
+      assert_equal TEST_DATA.fetch(:NO_LANGUAGE_TREE_SITTER_HTML), actual
     end
 
     def test_that_it_can_use_no_inline_highlighting_with_tree_sitter_enabled
       actual = convert_to_html(
-        NO_LANGUAGE_TREE_SITTER_INLINE_MARKDOWN,
+        TEST_DATA.fetch(:NO_LANGUAGE_TREE_SITTER_INLINE_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal NO_LANGUAGE_TREE_SITTER_INLINE_HTML, actual
+      assert_equal TEST_DATA.fetch(:NO_LANGUAGE_TREE_SITTER_INLINE_HTML), actual
     end
 
     def test_that_it_fails_gracefully_if_unable_to_locate_tree_sitter_parsers_directory
       actual = assert_raises Exception do
         convert_to_html(
-          PYTHON_TREE_SITTER_MARKDOWN,
+          TEST_DATA.fetch(:PYTHON_TREE_SITTER_MARKDOWN),
           :'tree-sitter',
           { tree_sitter_parsers_dir: FAKE_PARSERS_PATH }
         )
@@ -185,12 +168,12 @@ module Kramdown
 
     def test_that_it_can_use_tree_sitter_highlighting_with_rouge_identifiers
       actual = convert_to_html(
-        PYTHON_STANDARD_MARKDOWN,
+        TEST_DATA.fetch(:PYTHON_STANDARD_MARKDOWN),
         :'tree-sitter',
         { tree_sitter_parsers_dir: REAL_PARSERS_PATH }
       )
 
-      assert_equal PYTHON_TREE_SITTER_ROUGE_IDENTIFIER_HTML, actual
+      assert_equal TEST_DATA.fetch(:PYTHON_TREE_SITTER_ROUGE_IDENTIFIER_HTML), actual
     end
   end
 end
